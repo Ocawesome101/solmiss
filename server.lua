4082ce8a05cde4d66e3fde77827b29f10296f083
Ocawesome101 <ocawesome101@gmail.com>
2022-08-17 18:50:07 -0400

    whoops, fix bad slot stuff

--- server.lua
+++ server.lua
@@ -214,7 +214,7 @@ function api.deposit(io, ...)
 
             if item.count > 0 and not did_deposit then
               reason = "NO SLOT FOUND"
-              for i=1, #slots, 1 do
+              for i=1, slots.size, 1 do
                 if slots[i] == nil then
                   should_break = false
 
